a0ec3efbc486eba812b57aef21bd1ece5949a33c
Aaron Gullickson <aarongullickson@gmail.com>
2026-01-29 07:46:24 -0800

    Add start of making pictures lab

---
+++ week4_make_pictures.R
@@ -0,0 +1,18 @@
+# Making Pictures
+# Soc 412/512
+# Winter 2026
+
+
+# Load stuff --------------------------------------------------------------
+
+# load packages
+library(tidyverse)
+#library(ggplot2) - don't need this because its part of the tidyverse
+
+# load data
+load(url("https://github.com/AaronGullickson/practical_analysis/raw/master/data/nyc.RData"))
+
+
+# Make a picture ----------------------------------------------------------
+
+
