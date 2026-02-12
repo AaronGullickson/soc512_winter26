ad583ae3872dcf4a361224f9c7039aa56150c391
Aaron Gullickson <aarongullickson@gmail.com>
2026-02-12 13:39:53 -0800

    Read acs data

--- week6_read_data.R
+++ week6_read_data.R
@@ -25,3 +25,23 @@ read_fwf("example_data/data_fwf.txt",
                                   #race = c(16, 20),
                                   gender = c(21, 26),
                                   yrsed = c(27, 28)))
+
+# lets deal with IPUMS data
+#SEX                          P  53              1     X       
+#AGE                          P  54-56           3     X       
+#MARST                        P  57              1     X       
+#RACE                         P  58              1     X 
+
+acs_data <- read_fwf("example_data/usa_00131.dat.gz",
+                     col_positions = fwf_cols(sex = 53,
+                                              age = c(54, 56),
+                                              marst = 57,
+                                              race = 58,
+                                              cluster = c(24, 36)),
+                     col_types = cols(.default = "i", cluster = "d"))
+
+
+# Read in binary data -----------------------------------------------------
+
+
+load("stat_data/popularity.RData")
