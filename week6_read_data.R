7cb74667265384d11e2bde9614a971c91a5fbed0
Aaron Gullickson <aarongullickson@gmail.com>
2026-02-12 13:21:09 -0800

    Add fixed width data and code to read in

--- week6_read_data.R
+++ week6_read_data.R
@@ -15,3 +15,13 @@ my_data <- read_csv("example_data/data.csv")
 my_data <- read_csv("example_data/messy_data.csv", 
                     comment = "*", skip = 3)
 
+
+# Fixed width Files -------------------------------------------------------
+
+
+read_fwf("example_data/data_fwf.txt", 
+         col_positions = fwf_cols(name = c(1,5),
+                                  location = c(6, 15),
+                                  #race = c(16, 20),
+                                  gender = c(21, 26),
+                                  yrsed = c(27, 28)))
