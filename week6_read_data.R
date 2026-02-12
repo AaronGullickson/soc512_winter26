9b6b9e2d500ed10c3edb0b15d1b8b5222c3fcb93
Aaron Gullickson <aarongullickson@gmail.com>
2026-02-12 12:43:00 -0800

    Start week 6 script

---
+++ week6_read_data.R
@@ -0,0 +1,14 @@
+# Read Data into R
+# Soc 412/512, Winter 2026
+# Aaron Gullickson
+
+# Load libraries ----------------------------------------------------------
+
+library(readr)
+library(haven)
+library(readxl)
+
+# CSV Files ---------------------------------------------------------------
+
+
+
